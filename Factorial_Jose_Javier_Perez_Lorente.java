c6db299e89cd42fe35aa3117bce9d25ce5ecd3ad
Jose Javier Perez Lorente <javiperez81@hotmail.com>
2017-05-11 09:32:03 +0200

    Aprendiendo git

---
+++ Factorial_Jose_Javier_Perez_Lorente.java
@@ -0,0 +1,35 @@
+/** 
+* Clase que calcula el factorial de un número. 
+* @author José Javier Pérez Lorente 
+* @version 2017 
+*/
+
+public class Factorial {
+    
+	/** 
+	 * Calcula el factorial de n. 
+	 * N! = N * (n-1) * (n-2) * (n-3) * ... * 1 
+	 * @param n es el número al que se calculará el factorial. 
+	 * @return n! es el resultado del factorial de n 
+	 */
+	
+	private static double compruebaNumero(double numero) {
+		if (numero==0) {
+			return 1;
+		} else {
+			double resultat = numero * factorial(numero-1);
+			return resultat;
+		}
+	}
+
+	public static double factorial (double numero) {     
+		return compruebaNumero(numero);
+	}		
+     
+	public static void main(String[] args) {
+     
+		System.out.println(factorial(5));
+     
+	}
+     
+}
